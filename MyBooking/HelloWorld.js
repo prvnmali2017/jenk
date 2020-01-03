c0858861f957c545ffef8c66ff89253a88b4adf1
Praveen Mali <prvnmali@gmail.com>
2020-01-03 21:24:04 +1100

    Update HelloWorld.js

--- MyBooking/HelloWorld.js
+++ MyBooking/HelloWorld.js
@@ -1,14 +1,8 @@
-var http = require("http");
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let expect = chai.expect;
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
+chai.use(chaiHttp);
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/')
+
+console.log('Running')
